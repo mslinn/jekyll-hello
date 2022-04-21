9572887bedae307a33e41195e506f7f090dcb3fc
Mike Slinn <mslinn@mslinn.com>
2022-04-21 10:35:52 -0400

    -

--- lib/jekyll/hello.rb
+++ lib/jekyll/hello.rb
@@ -8,7 +8,7 @@ class Hello < Jekyll::Command
     def init_with_program(prog)
       prog.command(:hello) do |c|
         c.action do |args, options|
-          Jekyll.logger.info "Hello!"
+          Jekyll.logger.info "Hello! args=#{args}; options=#{options}"
         end
       end
     end
